a031f518f8b1f5009870d944dc8a886c50b5fe1d
MaikeruDev <michaelrimbach51@gmail.com>
2026-02-10 16:11:56 +0100

    updated WatchlistRepository

--- backend/src/main/java/org/example/backend/repositories/WatchlistRepository.java
+++ backend/src/main/java/org/example/backend/repositories/WatchlistRepository.java
@@ -2,6 +2,8 @@ package org.example.backend.repositories;
 
 import org.example.backend.models.Watchlist;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WatchlistRepository extends MongoRepository<Watchlist, String> {
 }
