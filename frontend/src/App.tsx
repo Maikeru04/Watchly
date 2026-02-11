a65856faef1828fbfa4a469e896ad9d7c8a2fbcb
MaikeruDev <michaelrimbach51@gmail.com>
2026-02-11 12:23:59 +0100

    updated App and Homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,35 @@
 import axios from "axios";
-import {useEffect, useState} from "react";
-import {Route, Routes} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Homepage from "./Homepage.tsx";
 import Watchlists from "./Watchlists.tsx";
 import ProtectedRoute from "./ProtectedRoute.tsx";
 import Navbar from "./Navbar.tsx";
+import type { Movie } from "./types/Movie.ts";
 
 function App() {
-
-
-    const [user, setUser] = useState<string | undefined | null>(undefined)
-
-    function login() {
-        const host:string = window.location.host === "localhost:5173" ? "http://localhost:8080" : window.location.origin
-        window.open(host + "/oauth2/authorization/github", "_self")
-    }
-
-    function logout() {
-        const host:string = window.location.host === "localhost:5173" ? "http://localhost:8080" : window.location.origin
-        window.open(host + "/logout", "_self")
-    }
-
-    function loadUser() {
-        axios.get("/api/auth")
-            .then(r => {
-                setUser(r.data)
-                console.log(r.data)
-            })
-            .catch(() => setUser(null))
-
-    }
+    const [user, setUser] = useState<string | undefined | null>(undefined);
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        loadUser()
-    }, [user]);
+        axios
+            .get("/api/auth")
+            .then((r) => setUser(r.data))
+            .catch(() => setUser(null));
+    }, []);
 
     return (
-    <>
-        <Navbar/>
-        <button onClick={login}>Login</button>
-        <button onClick={logout}>Logout</button>
-        <Routes>
-            <Route path={"/"} element={<Homepage/>}/>
-            <Route element={<ProtectedRoute user={user}/>}>
-                <Route path={"/watchlist"} element={<Watchlists/>}/>
-            </Route>
-        </Routes>
-    </>
-  )
+        <>
+            <Navbar setMovies={setMovies} setLoading={setLoading} />
+            <Routes>
+                <Route path={"/"} element={<Homepage movies={movies} loading={loading} />} />
+                <Route element={<ProtectedRoute user={user} />}>
+                    <Route path={"/watchlist"} element={<Watchlists />} />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
