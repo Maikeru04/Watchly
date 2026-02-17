37c52779d575e6def8554a0b461cbc16c55a59c4
MaikeruDev <michaelrimbach51@gmail.com>
2026-02-17 10:33:19 +0100

    Added "Add to Watcghlist" button & logic to MovieCard

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -1,10 +1,14 @@
 import type {Movie} from "../types/Movie.ts";
+import axios from "axios";
 
 type MovieCardProps = {
     movie: Movie
 }
 
 export default function MovieCard({movie}:MovieCardProps) {
+    function addToWatchlist() {
+        axios.post(`/api/watchlist/ee5647e1-1f2a-4a09-a1c6-d65f2d6d455e/movie/${movie.id}`)
+    }
     return(
             <div>
                 <h3>{movie.title}</h3>
@@ -17,6 +21,7 @@ export default function MovieCard({movie}:MovieCardProps) {
                         alt={movie.title}
                     />
                 )}
+                <button onClick={addToWatchlist}>Add to Watchlist</button>
             </div>
 
     )
