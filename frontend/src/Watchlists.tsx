035349aed8b18a81cee0a6ffb60bc5a86b7e7a91
MaikeruDev <michaelrimbach51@gmail.com>
2026-02-18 10:59:24 +0100

    initial Watchlist commit

--- frontend/src/Watchlists.tsx
+++ frontend/src/Watchlists.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
+
 export default function Watchlists() {
+    function testIt() {
+        axios.delete("/api/watchlist/ee5647e1-1f2a-4a09-a1c6-d65f2d6d455e/movie/20")
+    }
+
     return(
         <>
             <h1>Watchlists</h1>
+            <button onClick={testIt}>click ma</button>
         </>
     )
 }
