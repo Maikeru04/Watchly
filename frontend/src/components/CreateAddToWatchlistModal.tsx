5e74cbcffd2d939919662e519aa406ffba999e20
MaikeruDev <michaelrimbach51@gmail.com>
2026-02-26 16:32:17 +0100

    Updated Watchlist creation to check if loged in

--- frontend/src/components/CreateAddToWatchlistModal.tsx
+++ frontend/src/components/CreateAddToWatchlistModal.tsx
@@ -18,22 +18,44 @@ export default function CreateAddToWatchlistModal({isOpen, onClose, onCreated, m
     const [error, setError] = useState<string | null>(null);
     const [selected, setSelected] = useState("");
     const [watchlists, setWatchlists] = useState<Watchlist[]>([])
+    const [user, setUser] = useState<{ name: string } | null>(null);
+
+    if(!user) {
+
+    }
 
     const options:string[] = watchlists.map((watchlist) => (
-            watchlist.name
-        ))
+        watchlist.name
+    ))
+
+
+    const host =
+        window.location.host === "localhost:5173"
+            ? "http://localhost:8080"
+            : window.location.origin;
 
     if (!isOpen) return null;
 
-    useEffect(() => {
-        axios.get("/api/watchlist/user", { withCredentials: true })
-            .then(res => setWatchlists(res.data))
-            .catch(err => console.error(err));
-    }, []);
-
     const handleSave = async () => {
 
+        if (!selected) {
+            setError("Bitte wähle eine Watchlist aus.");
+            return;
+        }
+
+
         const watchlistToUpdate = watchlists.find(w => w.name === selected);
+
+        if (!watchlistToUpdate) {
+            setError("Watchlist nicht gefunden.");
+            return;
+        }
+
+        if(watchlistToUpdate.itemIDs.includes(`${movie.id}`)) {
+            setError("Dieser Film ist bereits in der ausgewählten Watchlist.")
+            return;
+        }
+
         try {
             await axios.post(`/api/watchlist/${watchlistToUpdate?.id}/movie/${movie.id}`,
                 {
@@ -49,35 +71,85 @@ export default function CreateAddToWatchlistModal({isOpen, onClose, onCreated, m
             }
         } catch (err) {
             console.error(err);
+            setError("Fehler beim Speichern.");
+        } finally {
+            setLoading(false);
         }
     };
 
+    function login() {
+        window.location.href = host + "/oauth2/authorization/github";
+    }
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        axios
+            .get(host + "/api/auth/me", { withCredentials: true })
+            .then(res => setUser(res.data))
+            .catch(() => setUser(null));
+    }, []);
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        if (!user) {
+            setWatchlists([]);
+            return;
+        }
+
+        axios.get("/api/watchlist/user", { withCredentials: true })
+            .then(res => {
+                console.log("Watchlists Data:", res.data);
+                setWatchlists(res.data);
+            })
+            .catch(err => console.error(err));
+
+    }, [user]);
+
     return (
+        <>
+        {user &&
+
         <div className="modal-overlay" onClick={onClose}>
-            <div className="modal" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-addwatchlist" onClick={(e) => e.stopPropagation()} style={{
+                backgroundImage: movie.poster_path
+                    ? `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`
+                    : `url(/image_not_found.jpg)`
+            }}>
+                <div className="modal-footer-group">
+                    <CustomDropdown
+                        options={options}
+                        selected={selected}
+                        onSelect={(value) => setSelected(value)}
+                        placeholder="Bitte Watchlist wählen!"
+                    />
 
-                <h2>Film zur Watchlist Hinzufügen:</h2>
+                    {error && <p style={{ color: "red" }}>{error}</p>}
 
-                <MovieCard movie={movie}/>
-
-                <CustomDropdown
-                    options={options}
-                    selected={selected}
-                    onSelect={(value) => setSelected(value)}
-                    placeholder="Bitte Watchlist wählen!"
-                />
-
-                {error && <p style={{ color: "red" }}>{error}</p>}
-
-                <div className={"modal-bottom"}>
-                    <button className={"btn"} onClick={onClose} disabled={loading}>
-                        Abbrechen
-                    </button>
-                    <button className={"btn"} onClick={handleSave} disabled={loading}>
-                        {loading ? "Speichern..." : "Speichern"}
-                    </button>
+                    <div className={"modal-bottom"}>
+                        <button className={"btn-secondary"} onClick={onClose} disabled={loading}>
+                            Abbrechen
+                        </button>
+                        <button className={"btn-secondary"} onClick={handleSave} disabled={loading}>
+                            {loading ? "Speichern..." : "Speichern"}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
+        }
+
+        {!user &&
+            <div className="modal-overlay" onClick={onClose}>
+                <div className="modal-login" onClick={(e) => e.stopPropagation()}>
+
+                    <h2>Bitte melde dich an!</h2>
+
+                    <button className="btn-secondary" onClick={login}>
+                        Login with Github
+                    </button>
+                </div>
+            </div>
+        }
+        </>
     )
 }
